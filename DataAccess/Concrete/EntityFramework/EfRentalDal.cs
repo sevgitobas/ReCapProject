6c1ac17e2585b1b2d2da4636ef6402b4d1636603
SEVGİ <SEVG@DESKTOP-FLEASTC>
2021-02-13 16:44:12 +0300

    ..

--- DataAccess/Concrete/EntityFramework/EfRentalDal.cs
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -1,13 +1,39 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfRentalDal: EfEntityRepositoryBase<Rental, RentCarContext>, IRentalDal
+    public class EfRentalDal : EfEntityRepositoryBase<Rental, RentCarContext>, IRentalDal
     {
+        public List<RentalDetailDto> GetRentalDetails(Expression<Func<Rental, bool>> filter = null)
+        {
+            using (RentCarContext context = new RentCarContext())
+            {
+                var result = from re in filter is null ? context.Rentals : context.Rentals.Where(filter)
+                             join ca in context.Cars
+                             on re.CarId equals ca.Id
+                             join cus in context.Customers
+                             on re.CustomerId equals cus.Id
+                             join us in context.Users
+                             on cus.UserId equals us.Id
+                             select new RentalDetailDto
+                             {
+                                 Id = re.CarId,
+                                 CompanyName = cus.CompanyName,
+                                 FirstName = us.FirstName,
+                                 LastName = us.LastName,
+                                 RentDate = re.RentDate,
+                                 ReturnDate = re.ReturnDate
+                             };
+                return result.ToList();
+            }
+        }
     }
 }
--- Entities/DTOs/RentalDetailDto.cs
+++ Entities/DTOs/RentalDetailDto.cs
@@ -3,13 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Concrete
+namespace Entities.DTOs
 {
-    public class Rental:IEntity
+    public class RentalDetailDto:IDto
     {
         public int Id { get; set; }
-        public int CarId { get; set; }
-        public int CustomerId { get; set; }
+        public string CompanyName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public DateTime RentDate { get; set; }
         public DateTime? ReturnDate { get; set; }
     }
