a71fd4a1b226f4f7a26401f6a5b9a74d4fb1ead9
SEVGİ <SEVG@DESKTOP-FLEASTC>
2021-03-05 15:11:11 +0300

    5 mart 2021

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,6 +1,9 @@
 ﻿using Business.Abstract;
+using Business.BusinessAspects.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofact.Caching;
+using Core.Aspects.Autofact.Transaction;
 using Core.Aspects.Autofact.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -21,7 +24,9 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [SecuredOperation("car.add,admin")]
         [ValidationAspect(typeof(CarValidator))]
+        [CacheRemoveAspect("ICarService.Get")]
         public IResult Add(Car car)
         {
             
@@ -30,12 +35,21 @@ namespace Business.Concrete
             return new SuccessResult(Messages.CarAdded);
         }
 
+        [TransactionScopeAspect]
+        public IResult AddTransactionalTest(Car car)
+        {
+            _carDal.Update(car);
+            _carDal.Add(car);
+            return new SuccessResult(Messages.CarUpdated);
+        }
+
         public IResult Delete(Car car)
         {
             _carDal.Delete(car);
             return new SuccessResult(Messages.CarDeleted);
         }
 
+        [CacheAspect]
         public IDataResult<List<Car>> GetAll()
         {
             if (DateTime.Now.Hour==22)
@@ -71,6 +85,7 @@ namespace Business.Concrete
         }
 
         [ValidationAspect(typeof(CarValidator))]
+        [CacheRemoveAspect("ICarService.Get")]
         public IResult Update(Car car)
         {
             _carDal.Update(car);
---
+++ Core/Aspects/Autofact/Transaction/TransactionScopeAspect.cs
@@ -0,0 +1,29 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+using Core.Utilities.Interceptors;
+
+namespace Core.Aspects.Autofact.Transaction
+{
+    public class TransactionScopeAspect : MethodInterception
+    {
+        public override void Intercept(IInvocation invocation)
+        {
+            using (TransactionScope transactionScope = new TransactionScope())
+            {
+                try
+                {
+                    invocation.Proceed();
+                    transactionScope.Complete();
+                }
+                catch (System.Exception e)
+                {
+                    transactionScope.Dispose();
+                    throw;
+                }
+            }
+        }
+    }
+}
